c7437f3201570555b4ae997ce5ff6316167b6be3
Molly <dngwoodo@inflab.com>
2023-09-24 19:44:11 +0900

    5장 유사한 코드 융합하기 #8
    복잡한 조건 통합하기

--- index.ts
+++ index.ts
@@ -562,12 +562,13 @@ function updateMap() {
 }
 
 function updateTile(x: number, y: number) {
-  if ((map[y][x].isStony()) && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Stone(new Falling());
-    map[y][x] = new Air();
-  } else if ((map[y][x].isBoxy()) && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Box(new Falling());
-    map[y][x] = new Air();
+  if (
+    ((map[y][x].isStony()) && map[y + 1][x].isAir()) || 
+    ((map[y][x].isBoxy()) && map[y + 1][x].isAir())
+  ) {
+    map[y][x].drop(); // 돌이나 상자를 떨어트리고(falling 상태 변경)
+    map[y + 1][x] = map[y][x]; // 타일을 교체한 후
+    map[y][x] = new Air(); // 새로 공기를 주입
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
   }
